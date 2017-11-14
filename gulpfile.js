580cb1afb468e7c53dbbafcbb11a82c21ad774be
Yahair <yajair@gmail.com>
2017-11-14 00:36:43 -0300

    Add | Ambiente de desarrollo

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,42 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+/* Actualiza el css en build/css cuando hay cambios en el scss de src/scss */
+gulp.task('css', function () {
+    console.log('Cambios en el scss actualiza css...');
+    gulp.src('source/scss/*.scss')
+        .pipe(sass({ outputStyle: "compressed" }))
+        .pipe(gulp.dest('build/css'));
+});
+
+/* Javascript minified  */
+gulp.task('minify', function () {
+    gulp.src('source/js/*.js')
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+});
+
+/* Browsersync */
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        }
+    });
+});
+
+gulp.task('watch-js', ['browserSync'], function () {
+    gulp.watch('build/index.html', browserSync.reload); //  Actualiza automaticamente el index
+    gulp.watch('build/css/*.css', browserSync.reload);	//  Actualiza automaticamente la carpeta css
+    gulp.watch('source/js/*.js', ['minify']);  // Automatización del js, no es necesario correr gulp para actualizar el archivo minificado
+    gulp.watch('source/scss/**/*.scss', ['css']); // Automatización del css, no es necesario correr gulp de nuevo
+});
 
 /* Build que compila */
-gulp.task('build', function () {
-    gulp.src('source/js/*.js')
-    .pipe(concat('scripts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js/'))
+gulp.task('build', ['watch-js', 'css'], function () {
+    console.log('Gulp se está ejecutando...');
 });
---
+++ source/js/scripts.js
@@ -0,0 +1,5 @@
+// contenido del archivo 1.js
+
+var sumar = function (g, d) {
+    return g + d;
+};
