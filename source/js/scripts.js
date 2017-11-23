3cedaf46ab9e038409a68646239127d7b4cd912c
Yahair <yajair@gmail.com>
2017-11-23 00:08:50 -0300

    Add | Navegacion index a silos

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -7,3 +7,19 @@ $('.btn-home').click(function () {
     $('.home').removeClass('show').addClass('hide'); 
     $('.index').removeClass('hide').addClass('show');
 });
+$('.btn-mercurial').click(function () {
+    $('.mercurial').removeClass('hide').addClass('show');
+    $('.index').removeClass('show').addClass('hide');
+});
+$('.btn-hypervenom').click(function () {
+    $('.hypervenom').removeClass('hide').addClass('show');
+    $('.index').removeClass('show').addClass('hide');
+});
+$('.btn-magista').click(function () {
+    $('.magista').removeClass('hide').addClass('show');
+    $('.index').removeClass('show').addClass('hide');
+});
+$('.btn-tiempo').click(function () {
+    $('.tiempo').removeClass('hide').addClass('show');
+    $('.index').removeClass('show').addClass('hide');
+});
