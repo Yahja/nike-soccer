a1bca0c98e41d875ea0dd2de945d26094b42e454
Yahair <yajair@gmail.com>
2017-11-29 00:59:30 -0300

    Del | Remove animation ui

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,10 +2,15 @@
 $('.btn-intro').click(function () {
     $('.intro').removeClass('show').addClass('hide');
     $('.home').removeClass('hide').addClass('show');
+    $('.title-home').addClass('animated fadeIn');
 });
 $('.btn-home').click(function () {
     $('.home').removeClass('show').addClass('hide'); 
     $('.index').removeClass('hide').addClass('show');
+    $('.btn-mercurial').addClass('animated fadeIn');
+    $('.btn-hypervenom').addClass('animated fadeIn');
+    $('.btn-magista').addClass('animated fadeIn');
+    $('.btn-tiempo').addClass('animated fadeIn');
 });
 $('.btn-mercurial').click(function () {
     $('.mercurial').removeClass('hide').addClass('show');
