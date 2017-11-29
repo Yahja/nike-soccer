450578c127ce90303ff9cd4c6cb54d888ed83067
Yahair <yajair@gmail.com>
2017-11-28 23:52:36 -0300

    Add | Motion UI

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -9,17 +9,21 @@ $('.btn-home').click(function () {
 });
 $('.btn-mercurial').click(function () {
     $('.mercurial').removeClass('hide').addClass('show');
+    $('.menu-global').removeClass('hide').addClass('show');
     $('.index').removeClass('show').addClass('hide');
 });
 $('.btn-hypervenom').click(function () {
     $('.hypervenom').removeClass('hide').addClass('show');
+    $('.menu-global').removeClass('hide').addClass('show');
     $('.index').removeClass('show').addClass('hide');
 });
 $('.btn-magista').click(function () {
     $('.magista').removeClass('hide').addClass('show');
+    $('.menu-global').removeClass('hide').addClass('show');
     $('.index').removeClass('show').addClass('hide');
 });
 $('.btn-tiempo').click(function () {
     $('.tiempo').removeClass('hide').addClass('show');
+    $('.menu-global').removeClass('hide').addClass('show');
     $('.index').removeClass('show').addClass('hide');
 });
